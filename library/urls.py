e52d4ec1d8dfdecbe2f9a6f69ff83bd6d2e9213c
delyasss <onlasynadele@gmail.com>
2026-03-05 00:33:17 +0500

    Добавила стили

--- library/urls.py
+++ library/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.books_list, name='books_list'),
+    path('authors/', views.authors_list, name='authors_list'),
+    path('readers/', views.readers_list, name='readers_list'),
 ]
--- library/views.py
+++ library/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from .models import Book
+from .models import Book, Author, Reader
 
 
 def books_list(request):
-    """Задание 5: все книги + связанные автор и жанры (обратные связи)."""
     books = Book.objects.all().select_related('author').prefetch_related('genres')
     return render(request, 'library/books_list.html', {'books': books})
+
+
+def authors_list(request):
+    authors = Author.objects.all().prefetch_related('books')
+    return render(request, 'library/authors_list.html', {'authors': authors})
+
+
+def readers_list(request):
+    readers = Reader.objects.all().select_related('profile').prefetch_related('loans__book')
+    return render(request, 'library/readers_list.html', {'readers': readers})
