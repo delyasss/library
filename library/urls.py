213432ec97caeaa54af26bda6c1cf1997cdc62d1
delyasss <onlasynadele@gmail.com>
2026-03-05 00:14:11 +0500

    first commit

---
+++ library/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Маршруты добавим в Задании 5
+]
---
+++ library/views.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render
+
+# View для Задания 5 добавим позже
